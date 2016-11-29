1c5a87ce10da80285b521120ac25bf8252f11a84
distres117 <omcrobbie@gmail.com>
2016-11-28 22:27:47 -0500

    averaging values

--- app/components/chartPane.jsx
+++ app/components/chartPane.jsx
@@ -6,14 +6,30 @@ class ChartPane extends Component {
     constructor(props) {
         super(props);
     }
+    getDataPoint(e){
+        console.log(e.target);
+    }
 
     makeData(readings) {
         if (readings.length) {
-            //split into 20 groups
+            //split into 30 groups
+            let groupSize = Math.floor(readings.length /30);
             //calculate average of each group
+            let dataPoints = [];
+            for(let i=0;i<readings.length;i+=groupSize){
+                let j = i;
+                let group = [];
+                while(j<i+groupSize){
+                    let reading = readings[j];
+                    if (reading !== undefined)
+                        group.push(parseInt(reading.value));
+                    j++;
+                }
+                let avg = Math.round(group.reduce((ac,n)=>ac+=n,0) / group.length);
+                dataPoints.push(avg);
+            }
             let device = this.props.device;
-            let dataPoints = readings.map(r=>parseInt(r.value || 0));
-            let labels = ['start', 'finish'];
+            let labels = dataPoints.map((d,i)=>i)
             let data = {
                 labels: labels,
                 datasets: [
@@ -28,15 +44,17 @@ class ChartPane extends Component {
                  
                 ]
             };
-            console.log(min,max);
             let options = {
                 responsive: true,
-                scaleSteps:10
             };
             return (
                 <div>
-                    <h4>{device.name}</h4>
-                    <LineChart data={data} options={options} height='250' width='1300' />                
+                    <h5><i className='fi-laptop'></i> {device.id}</h5>
+                    <span>{'Average '+ device.type + ' values'} </span>
+                    <LineChart onClick={this.getDataPoint} data={data} options={options} height='200' width='1300' />
+                    <div className='chart-subtitle'>
+                        <small>Click on a datapoint to view raw data...</small>
+                    </div>               
                 </div>
             )
         } else {
